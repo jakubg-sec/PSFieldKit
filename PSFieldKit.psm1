ce6d3bb3742938c169dac0df401be63bc10eeb02
jakubg <kuballo.g@gmail.com>
2026-09-16 20:23:00 +0200

    Refactor PSFieldKit Menus: Reorganize and Move Scripts to Appropriate Directories
    - Deleted old menu scripts from the Public directory.
    - Created new menu scripts in their respective directories:
      - ActiveDirectory: Show-ADMenu.ps1
      - ComputerInformation: Show-ComputerMenu.ps1
      - EventLogs: Show-EventLogMenu.ps1
      - NetworkDiagnostics: Show-NetworkMenu.ps1
      - ProcessesServices: Show-ProcessServiceMenu.ps1
      - RemoteAdministration: Show-RemoteAdministrationMenu.ps1
      - Security: Show-SecurityMenu.ps1
      - SoftwareUpdates: Show-SoftwareMenu.ps1
      - StorageDisks: Show-StorageMenu.ps1

--- PSFieldKit.psm1
+++ PSFieldKit.psm1
@@ -1,15 +1,27 @@
 # Public
-. $PSScriptRoot\public\Show-PSFieldKitMenu.ps1
-. $PSScriptRoot\public\Show-ComputerMenu.ps1
-. $PSScriptRoot\public\Show-NetworkMenu.ps1
-. $PSScriptRoot\public\Show-ADMenu.ps1
-. $PSScriptRoot\public\Show-ProcessServiceMenu.ps1
-. $PSScriptRoot\public\Show-EventLogMenu.ps1
-. $PSScriptRoot\public\Show-StorageMenu.ps1
-. $PSScriptRoot\public\Show-SecurityMenu.ps1
-. $PSScriptRoot\public\Show-RemoteAdministrationMenu.ps1
-. $PSScriptRoot\public\Show-SoftwareMenu.ps1
+. $PSScriptRoot\Public\Show-PSFieldKitMenu.ps1
+. $PSScriptRoot\Public\ComputerInformation\Show-ComputerMenu.ps1
+. $PSScriptRoot\Public\NetworkDiagnostics\Show-NetworkMenu.ps1
+. $PSScriptRoot\Public\ActiveDirectory\Show-ADMenu.ps1
+. $PSScriptRoot\Public\ProcessesServices\Show-ProcessServiceMenu.ps1
+. $PSScriptRoot\Public\EventLogs\Show-EventLogMenu.ps1
+. $PSScriptRoot\Public\StorageDisks\Show-StorageMenu.ps1
+. $PSScriptRoot\Public\Security\Show-SecurityMenu.ps1
+. $PSScriptRoot\Public\RemoteAdministration\Show-RemoteAdministrationMenu.ps1
+. $PSScriptRoot\Public\SoftwareUpdates\Show-SoftwareMenu.ps1
+
+# . $PSScriptRoot/public/Show-PSFieldKitMenu.ps1
+# . $PSScriptRoot/public/Show-ComputerMenu.ps1
+# . $PSScriptRoot/public/Show-NetworkMenu.ps1
+# . $PSScriptRoot/public/Show-ADMenu.ps1
+# . $PSScriptRoot/public/Show-ProcessServiceMenu.ps1
+# . $PSScriptRoot/public/Show-EventLogMenu.ps1
+# . $PSScriptRoot/public/Show-StorageMenu.ps1
+# . $PSScriptRoot/public/Show-SecurityMenu.ps1
+# . $PSScriptRoot/public/Show-RemoteAdministrationMenu.ps1
+# . $PSScriptRoot/public/Show-SoftwareMenu.ps1
 
 
 # Private
-. $PSScriptRoot\private\New-PSFieldKitContext.ps1
+. $PSScriptRoot\Private\New-PSFieldKitContext.ps1
+# . $PSScriptRoot/private/New-PSFieldKitContext.ps1
